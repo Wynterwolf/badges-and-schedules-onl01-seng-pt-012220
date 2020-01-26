d7c612253152c3d5290313e31d978ad172b188c4
Samantha Jordan <shinyguns@gmail.com>
2020-01-26 05:46:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,11 @@ def batch_badge_creator(attendees) #define method with array
         # names.map { |name| badge_maker(name) } <-- also valid for this step
     end
 end
+
+def assign_rooms(speakers)
+    room = 0 #start with no rooms
+    speakers.collect do |name| # collect info, call them name
+        room += 1 #add one each loop (because no one wants room 0)
+        return "Hello, #{name}! You'll be assigned to room #{room}!" #return phrase
+    end
+end
