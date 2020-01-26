c7e2302fa4bc75b975805e7dc4247eec3dc4585f
Samantha Jordan <shinyguns@gmail.com>
2020-01-26 05:46:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,10 @@ def badge_maker(name) #define method
     return "Hello, my name is #{name}." #return this
 end
 
+def batch_badge_creator(attendees) #define method with array
+    attendees.collect do |name| #collect and do, call them name
+        badge_maker(name) #take name and do badge_maker
+
+        # names.map { |name| badge_maker(name) } <-- also valid for this step
+    end
+end
